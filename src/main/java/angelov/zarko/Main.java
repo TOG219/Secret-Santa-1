9afe362f9e09464e857ef548e948c59ba4c673fe
TOG219 <zarko.ang.bg@gmail.com>
2024-12-10 22:52:29 +0000

    Added code

--- src/main/java/angelov/zarko/Main.java
+++ src/main/java/angelov/zarko/Main.java
@@ -2,6 +2,12 @@ package angelov.zarko;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Secret_Santa.AddToArray("Josh");
+        Secret_Santa.AddToArray("Jeff");
+        Secret_Santa.AddToArray("Mag");
+        Secret_Santa.AddToArray("Stroll");
+        Secret_Santa.AddToArray("Cooper");
+
+        System.out.println("Your draw is: "+Secret_Santa.Draw()+"!");
     }
 }
--- src/main/java/angelov/zarko/Secret_Santa.java
+++ src/main/java/angelov/zarko/Secret_Santa.java
@@ -1,4 +1,26 @@
 package angelov.zarko;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Secret_Santa {
+    static ArrayList<String> list = new ArrayList<>();
+
+    static Random random = new Random();
+
+    public static void AddToArray(String name)
+    {
+        list.add(name);
+    }
+
+    public static void PrintArray()
+    {
+        System.out.println(list);
+    }
+
+    public static String Draw()
+    {
+        int randomInt = random.nextInt(list.size());
+        return list.get(randomInt);
+    }
 }
